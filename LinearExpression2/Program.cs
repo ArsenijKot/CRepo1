6a276b31371d09cfe2205d12a29c1793b7289565
ArsenijKot <ipz241_kar@student.ztu.edu.ua>
2024-11-14 18:51:26 +0200

    Sana 1 add

---
+++ LinearExpression2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Enter values m, n, a, b, x:");
+        double m = double.Parse(Console.ReadLine());
+        double n = double.Parse(Console.ReadLine());
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double x = double.Parse(Console.ReadLine());
+
+        
+        double z1 = ((m - 1) * Math.Sqrt(m) - (n - 1) * Math.Sqrt(n)) /
+                    (Math.Sqrt(Math.Pow(m, 3) * n + n * m + Math.Pow(m, 2) - m));
+
+        double z2 = (Math.Sqrt(m) - Math.Sqrt(n)) / m;
+
+        double y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) +
+                   (a - b) * Math.Pow(Math.Sin(a - b), 2) +
+                   Math.Pow(10, -2) * (x - b);
+
+        
+        Console.WriteLine($"z1 = {z1}");
+        Console.WriteLine($"z2 = {z2}");
+        Console.WriteLine($"y = {y}");
+    }
+}
---
+++ Sana01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Enter values a, b, c, d:");
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+        double d = double.Parse(Console.ReadLine());
+
+        
+        double x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (a * a) / (b * b);
+        double y = 5 * (a + b) * (c - d) / (0.5 * c) + (d * d) * (a * a - b * b) / (b - a);
+        double z = ((x * x - 2 * x) * (x * x - 2 * x) * (x * x - 2 * x) - 4 * (x * x * x * x + 1) * (1 - b)) / (5 * a + 3 * b);
+        double r = (0.5 * a + (3.0 / 4.0) * b - 7.0 / 5.0) / (3 * c + 1) + 1 / (a - c);
+
+        
+        Console.WriteLine($"x = {x}");
+        Console.WriteLine($"y = {y}");
+        Console.WriteLine($"z = {z}");
+        Console.WriteLine($"r = {r}");
+    }
+}
